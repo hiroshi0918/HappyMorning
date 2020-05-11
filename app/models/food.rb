04c50d966bc1024ef69d4535c4f7b4236bb5374f
Hiroshi Terashima <terahiro0918@icloud.com>
2020-05-11 11:39:33 +0900

    訂正

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,7 +1,7 @@
 class Food < ApplicationRecord
   belongs_to :category
-  has_many :user_items
+  has_many :user_items, dependent: :destroy
   has_many :users, through: :user_items
-  has_many :order_items
+  has_many :order_items, dependent: :nullify
   belongs_to :shop
 end
