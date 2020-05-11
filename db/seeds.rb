be9c6ad506ea6133afc1c6146ad8dc2096a552d4
Hiroshi Terashima <terahiro0918@icloud.com>
2020-05-11 10:19:46 +0900

    seed 訂正

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,8 @@ Food.create([{ name: 'クロワッサン', price: '250',category_id: '1',image:'
   { name: 'バゲット', price: '200',category_id: '1' ,image:'バゲット.jpg',shop_id: '1'},
   { name: 'カレーパン', price: '300',category_id: '1' ,image:'カレーパン.jpg',shop_id: '2'},
   { name: 'BLTサンド', price: '300',category_id: '1' ,image:'BLTサンド.jpeg',shop_id: '2'},
-  { name: 'コーヒーパン', price: '300',category_id: '1' ,image:'コーヒーパン.jpg',shop_id: '3'},
+  { name: 'コーヒーパン', price: '300',category_id: '1' ,image:'コーヒーパン.jpg',shop_id: '3'}
   ])
-
 Shop.create([{ name: 'ベーカリー', address: '東京都港区南麻布１丁目',telephone: '08012345678',image: 'ベーカリー.jpg' }, 
   { name: 'サンモリッツ', address: '東京都港区南麻布３丁目',telephone: '08012344321',image: 'サンモリッツ.jpg' },
   { name: 'テラコーヒー', address: '横浜市神奈川区白楽129番地',telephone: '045-309-8686',image: 'テラコーヒー.jpg' }])
