ef5509fd29b64e472673109617f8d1262022db6f
Hiroshi Terashima <terahiro0918@icloud.com>
2020-05-12 17:49:24 +0900

    master.key本番環境に設置

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,9 @@ set :application, 'HappyMorning'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:hiroshi0918/HappyMorning.git'
 
+#マスターキーをshared/config配下に設置
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
