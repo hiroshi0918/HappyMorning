59a646f04b0d0f8f1a197abcfea645fc9d2ee087
Hiroshi Terashima <terahiro0918@icloud.com>
2020-05-13 16:33:27 +0900

    turbolinks 修正

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,16 +1,16 @@
 document.addEventListener(
-  "DOMContentLoaded", e => {
+  "turbolinks:load", e => {
     if (document.getElementById("token_submit") != null) { //token_submitというidがnullの場合、下記コードを実行しない
       Payjp.setPublicKey("pk_test_d7aa23fb81953cbd6be4a2bd"); //ここに公開鍵を直書き
       let btn = document.getElementById("token_submit"); //IDがtoken_submitの場合に取得されます
       btn.addEventListener("click", e => { //ボタンが押されたときに作動します
-        e.preventDefault(); //ボタンを一旦無効化します
+        e.preventDefault(); //ボタンを一旦無効化
         let card = {
           number: document.getElementById("card_number").value,
           cvc: document.getElementById("cvc").value,
           exp_month: document.getElementById("exp_month").value,
           exp_year: document.getElementById("exp_year").value
-        }; //入力されたデータを取得します。
+        }; //入力されたデータを取得
         Payjp.createToken(card, (status, response) => {
           if (status === 200) { //成功した場合
             $("#card_number").removeAttr("name");
@@ -19,11 +19,11 @@ document.addEventListener(
             $("#exp_year").removeAttr("name"); //データを自サーバにpostしないように削除
             $("#card_token").append(
               $('<input type="hidden" name="payjp-token">').val(response.id)
-            ); //取得したトークンを送信できる状態にします
+            ); //取得したトークンを送信できる状態に
             document.inputForm.submit();
-            alert("登録が完了しました"); //確認用
+            alert("登録が完了しました");
           } else {
-            alert("カード情報が正しくありません。"); //確認用
+            alert("カード情報が正しくありません。");
           }
         });
       });
